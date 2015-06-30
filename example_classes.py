152ecf344e06b1d0246d88f0f95cb6dd3482f757
dangweili <dangweili@gmail.com>
2015-06-30 15:04:15 +0800

    add an example of class defination

---
+++ example_classes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+class Greeter:
+
+    # Constructor
+    def __init__(self, name):
+        self.name = name  # Create an instance variable
+
+    # Instance method
+    def greet(self, loud=False):
+        if loud:
+            print 'HELLO, %s!' % self.name.upper()
+        else:
+            print 'Hello, %s' % self.name
+
+g = Greeter('Fred')  # Construct an instance of the Greeter class
+g.greet()            # Call an instance method; prints "Hello, Fred"
+g.greet(loud=True)   # Call an instance method; prints "HELLO, FRED!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
