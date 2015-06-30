4b9ae2e2b6c3dffeffe6681baa1993273111fc54
dangweili <dangweili@gmail.com>
2015-06-30 15:03:47 +0800

    add some functions defination here

---
+++ example_functions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# basic fucntion defination
+def sign(x):
+    if x > 0:
+        return 'positive'
+    elif x < 0:
+        return 'negative'
+    else:
+        return 'zero'
+
+for x in [-1, 0, 1]:
+    print sign(x)
+    # Prints "negative", "zero", "positive"
+
+# optional parameter function defination
+def hello(name, loud=False):
+    if loud:
+        print 'HELLO, %s' % name.upper()
+    else:
+        print 'Hello, %s!' % name
+
+hello('Bob') # Prints "Hello, Bob"
+hello('Fred', loud=True)  # Prints "HELLO, FRED!"
+
+
+
+
+
+
+
