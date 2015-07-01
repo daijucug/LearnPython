568c119baa171ea9e142d67eaa5c5c75823b66ed
dangweili <dangweili@gmail.com>
2015-07-01 17:03:04 +0800

    image from scipy.misc imread, imresize, imsave

--- example_scipy.py
+++ example_scipy.py
@@ -22,5 +22,29 @@ img_tinted = imresize(img_tinted, (300, 300))
 imsave('/data1/NLPRMNT/lidangwei/cat_tinted.jpg', img_tinted)
 
 
+# scipy to compute the distance between points
+
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+# Create the following array where each row is a point in 2D space:
+# [[0 1]
+#  [1 0]
+#  [2 0]]
+x = np.array([[0, 1], [1, 0], [2, 0]])
+print x
+
+# Compute the Euclidean distance between all rows of x.
+# d[i, j] is the Euclidean distance between x[i, :] and x[j, :],
+# and d is the following array:
+# [[ 0.          1.41421356  2.23606798]
+#  [ 1.41421356  0.          1.        ]
+#  [ 2.23606798  1.          0.        ]]
+d = squareform(pdist(x, 'euclidean'))
+print d
+
+
+
+
 
 
